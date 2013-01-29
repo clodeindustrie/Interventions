f45563e476ab683f1aaa665e5c179ea26950c392
jeremy <jeremy.speissegger@affiliatewindow.com>
2013-01-29 10:15:22 +0000

    added pry, migrations

---
+++ scripts/3_create_agents.rb
@@ -0,0 +1,18 @@
+Sequel.migration do
+  up do
+    create_table(:agents) do
+      primary_key :id
+      foreign_key :role_id, :roles
+      String :nom, :null => false, :size => 100
+      String :email, :null => false, :size => 255
+      String :hashed_password, :null => false, :size => 255
+      String :salt, :null => false, :size => 255
+      DateTime :created_at, :null => false, :default => Time.now
+      index(:email)
+    end
+  end
+
+  down do
+    drop_table(:agents)
+  end
+end
---
+++ scripts/5_create_fiches.rb
@@ -0,0 +1,24 @@
+Sequel.migration do
+  up do
+    create_table(:fiches) do
+      primary_key :id
+      foreign_key :addresse_id, :addresses
+      foreign_key :demandeur_id, :agents
+      foreign_key :executant_id, :agents
+      foreign_key :statut_id, :statuts
+      String :sujet, :null => false, :size => 255
+      String :famille, :null => false, :size => 255
+      String :contenue, :null => false, :size => 1000
+      String :observations, :size => 1000
+      String :travaux, :size => 1000
+      DateTime :created_at, :null => false, :default => Time.now
+      DateTime :done_at
+      String :priority, :null => false, :default => 'U'
+      index :email, :unique => true
+    end
+  end
+
+  down do
+    drop_table(:fiches)
+  end
+end
